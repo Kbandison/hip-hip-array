2bf0da0234c8e2ac27cbe7cfba4357fe2849a043
Kevin Bandison <kbandison@gmail.com>
2022-09-27 14:41:15 -0400

    initial commit

--- main.js
+++ main.js
@@ -26,7 +26,12 @@ const students = [
   ];
   // Challenge 1 Code
 
+  let s = 0;
 
+  while (s < students.length) {
+    console.log(students[s]);
+    s++;
+  }
 
   console.log("\n");
   console.log("Challenge 2");
@@ -35,7 +40,13 @@ const students = [
   const grades = [100, 80, 110, 75, 83, 64];
   //Challenge 2 Code
 
-  
+  let g = 0;
+
+  while (g < grades.length) {
+    grades.reverse();
+    console.log(grades[g]);
+    g++;
+  }
 
 
   console.log("\n");
@@ -45,7 +56,14 @@ const students = [
   const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
   // Challenge 3 Code
 
+  let p = 0;
 
+  while (p < positiveNumbers.length) {
+    if (positiveNumbers[p] % 2 === 0) {
+      console.log(positiveNumbers[p]);
+    }
+    p++;
+  }
 
   console.log("\n");
   console.log("Challenge 4");
@@ -55,7 +73,14 @@ const students = [
 
   // Challenge 4 Code
 
+  let m = 0;
 
+  while (m < mixedSignNumbers.length) {
+    if (mixedSignNumbers[m] % 2 === 0) {
+      console.log(mixedSignNumbers[m]);
+    }
+    m++;
+  }
 
   
   console.log("\n");
@@ -64,8 +89,16 @@ const students = [
   // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
   const symmetricalCapitals = ['A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y'];
   // Challenge 5 Code
-    
+  let c = 0;
 
+  symmetricalCapitals.pop();
+  symmetricalCapitals.shift();
+  symmetricalCapitals.shift();  
+
+  while (c < symmetricalCapitals.length) {
+  console.log(symmetricalCapitals[c]);
+  c++;
+  }
 
   console.log("\n");
   console.log("Challenge 6");
@@ -74,7 +107,17 @@ const students = [
   const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
   // Challenge 6 Code
   
-  
+  let f = 0;
+
+    fibonacciNumbers.unshift(0);
+    fibonacciNumbers.push(21);
+    fibonacciNumbers.push(34);
+
+  while (f < fibonacciNumbers.length) {
+    console.log(fibonacciNumbers[f]);
+    f++;
+  }
+
 
 
   console.log("\n");
@@ -83,7 +126,14 @@ const students = [
   // Make a NEW array with 5 values of your choice, using the same syntax as the array creation from the previous 6 challenges; that is, every value should be placed within the square brackets.  Now console.log out each value individually.
   // Challenge 7 Code
 
+  const arr = ["Peter", "Stephen", "Steve", "Natasha", "Bucky", "Wanda"];
 
+  let a = 0;
+
+  while (a < arr.length) {
+    console.log(arr[a]);
+    a++;
+  }
 
 
 
@@ -94,6 +144,15 @@ const students = [
   const emptyArray = [];
   // Challenge 8 Code
 
+  let e = 0;
+
+  emptyArray.push("Sam", "Jennifer", "Loki");
+  emptyArray.unshift("Thor", "Odin");
+
+  while (e < emptyArray.length) {
+    console.log(emptyArray[e]);
+    e++;
+  }
 
 
 
@@ -103,6 +162,12 @@ const students = [
   // Loop through the `students` array from Challenge 1, starting at index 3 and ending at index 10 (INCLUSIVE), logging out each value individually.
   // Challenge 9 Code
   
+  let st = 3;
+  
+  while (st < 11) {
+    console.log(students[st]);
+    st++;
+  }
   
   console.log("\n");
   console.log("Challenge 10");
@@ -111,17 +176,35 @@ const students = [
   // Do NOT use slice, you will be using that for the next challenge!
   // Challenge 10 Code
 
+  let copy = [];
 
+  let stud = 3;
+  let cc = 0;
+  
+  while (stud < 11) {
+    copy.push(students[stud]);
+    stud++;
+  }
 
-
+  while (cc < copy.length) {
+    console.log(copy[cc]);
+    cc++;
+  }
+  
   console.log("\n");
   console.log("Challenge 11");
   // Challenge 11
   // Make a COPY of the `students` array *using .slice*, starting at index 3 and ending at index 10 (INCLUSIVE), then log out each value of the copy individually. THEN log out each value of the original array. Notice that the original array didn't get any values taken out of it, because `slice` does NOT modify the original array.
   // Challenge 11 Code
   
-
-
+  let stu = 0;
+  let newArr = students.slice(3,10);
+  
+  while (stu < newArr.length) {
+    console.log(newArr[stu]);
+    stu++;
+  }
+  console.log(students);
 
   console.log("\n");
   console.log("Challenge 12");
@@ -130,8 +213,14 @@ const students = [
   const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimetrodon', 'Allosaur', 'Spinosaurus', 'Gigantosaur']
   // Challenge 12 Code
 
-  
-  
+  let d = 0;
+
+  dinosaurs.splice(4,3);
+
+  while(d < dinosaurs.length) {
+    console.log(dinosaurs[d]);
+    d++;
+  }
   
   console.log("\n");
   console.log("Challenge 13");
@@ -139,17 +228,17 @@ const students = [
   // Use .join to change the dinosaur strings from the above array into one string with a '*' string as the "separator". Then console.log out that string.
   // Challenge 13 Code
 
-
+  console.log(dinosaurs.join(" * "));
 
   
   console.log("\n");
-
   console.log("Challenge 14");
   // Challenge 14
   // Use .reverse to reverse the dinosaur array and then log out the original array. .reverse DOES change the original array.
   // Challenge 14 Code
 
-
+  console.log(`Original array:\n ${dinosaurs}`);
+  console.log(`New array:\n ${dinosaurs.reverse()}`);
 
   
   console.log("\n");
@@ -159,5 +248,17 @@ const students = [
   // Use .concat to combine those two arrays into a NEW array you make (.concat RETURNS an array). Then console.log out each value of that new array individually. Now console.log both the primaries and secondaries arrays, noting that .concat does NOT mutate the original arrays.
   // Challenge 15 Code
 
+  let arrA = ["David", "Lucy", "Kiwi"];
+  let arrB = ["Maine", "Rebecca", "Gloria"];
 
+  let con = 0;
+
+  let cat = arrA.concat(arrB);
   
+  while (con < 6) {
+    console.log(cat[con]);
+    con++;
+  } 
+
+    console.log(arrA);
+    console.log(arrB);
---
+++ notes.md
@@ -0,0 +1,14 @@
+# Array Methods
+
+.reverse() - displays array in reverse order
+.push() - adds value to the end of the array
+.pop() - removes value from the end of the array
+.unshift() - adds value to the beginning of the array
+.shift() - removes value from the beginning of the array
+.indexOf() - checks to see if value inside parenthasis is inside of the array and displays the first one
+.lastIndexOf() - checks to see if value inside parenthasis is inside of the array and displays the last one
+.splice() - removes items from array starting from first index specified, and number of items after to remove
+.slice() - makes a copy of the array with the range specified
+.concat() - combines 2 arrays together
+.join() - converts array into a comma separated string
+.includes() - looks for specified value, returning true if value is present, false if not
